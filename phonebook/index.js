fb954085272dd53e602d535b3130789678d3d2d6
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-10-30 02:52:41 +0100

    npm init

---
+++ phonebook/index.js
@@ -0,0 +1,119 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(express.json());
+
+const cors = require("cors");
+
+app.use(cors());
+
+// Define a new token 'body' for logging the request body
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
+
+// Use morgan middleware with custom format
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+  {
+    id: 5,
+    name: "0x Ose",
+    number: "39-23-6423122",
+  },
+  {
+    id: 6,
+    name: "0se Ehimen",
+    number: "39-23-6423122",
+  },
+];
+
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  console.log(body);
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number missing",
+    });
+  }
+
+  const existingPerson = persons.find((person) => person.name === body.name);
+  if (existingPerson) {
+    return response.status(400).json({
+      error: "name already exists in the phonebook",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(body);
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/info", (request, response) => {
+  const date = new Date();
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+    <p>${date}</p>`
+  );
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+const port = 3001;
+app.listen(port);
+console.log(`Server running on port ${port}. Go to http://localhost:${port}`);
