c563f89d3e6c5cd78c7ea961205b8f0bc2db363c
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-10-30 22:17:11 +0100

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -12,6 +12,8 @@ const password = process.argv[2];
 
 // Constructing the connection string URL
 const url = `mongodb+srv://patrickehimen22:${password}@cluster0.91tuixm.mongodb.net/phoneBook?retryWrites=true&w=majority`;
+// 5dYBvMQ14Q0UxGJQ
+// mongodb+srv://patrickehimen22:<password>@cluster1.iydofkb.mongodb.net/?retryWrites=true&w=majority
 
 // Configuring mongoose
 mongoose.set("strictQuery", false);
@@ -20,37 +22,47 @@ mongoose.set("strictQuery", false);
 mongoose.connect(url);
 
 // Defining the schema for "PhonebookEntry"
-const phonebookEntrySchema = new mongoose.Schema({
+const phonebookSchema = new mongoose.Schema({
   name: String,
   number: String,
 });
 
 // Creating a model from the phonebook entry schema
-const PhonebookEntry = mongoose.model("PhonebookEntry", phonebookEntrySchema);
+const PhonebookEntry = mongoose.model("PhonebookEntry", phonebookSchema);
+
+const entry = new PhonebookEntry({
+  name: "Patrick",
+  number: "123",
+});
+
+entry.save().then((result) => {
+  console.log("Entry saved!");
+  mongoose.connection.close();
+});
 
 // Adding an entry to the phonebook
-if (process.argv.length === 5) {
-  const name = process.argv[3];
-  const number = process.argv[4];
+// if (process.argv.length === 5) {
+//   const name = process.argv[3];
+//   const number = process.argv[4];
 
-  const entry = new PhonebookEntry({
-    name,
-    number,
-  });
+//   const entry = new PhonebookEntry({
+//     name,
+//     number,
+//   });
 
-  entry.save().then((result) => {
-    console.log(`Added ${name} number ${number} to the phonebook.`);
-    mongoose.connection.close();
-  });
-}
+//   entry.save().then((result) => {
+//     console.log(`Added ${name} number ${number} to the phonebook.`);
+//     mongoose.connection.close();
+//   });
+// }
 
-// Listing all entries in the phonebook
-if (process.argv.length === 3) {
-  PhonebookEntry.find({}).then((entries) => {
-    console.log("Phonebook entries:");
-    entries.forEach((entry) => {
-      console.log(`${entry.name} ${entry.number}`);
-    });
-    mongoose.connection.close();
-  });
-}
+// // Listing all entries in the phonebook
+// if (process.argv.length === 3) {
+//   PhonebookEntry.find({}).then((entries) => {
+//     console.log("Phonebook entries:");
+//     entries.forEach((entry) => {
+//       console.log(`${entry.name} ${entry.number}`);
+//     });
+//     mongoose.connection.close();
+//   });
+// }
